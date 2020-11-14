955239d0210271bd9527050ef4df9ecb14a472e4
Omar HOSNI <hosni.omar.work@gmail.com>
2020-11-14 21:58:37 +0100

    done

--- src/movies.js
+++ src/movies.js
@@ -1,16 +1,53 @@
 // Iteration 1: All directors? - Get the array of all directors.
-// _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
 
-// Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
+function getAllDirectors(movies) {
+  return movies.map(movie => movie.director )
+  }
 
-// Iteration 3: All rates average - Get the average of all rates with 2 decimals
+// Iteration 2: Steven Spielberg. The best? - 
+// How many drama movies did STEVEN SPIELBERG direct?
+
+function howManyMovies (movies){
+  return movies.filter(movie => movie.genre.includes('Drama')
+   && movie.director === "Steven Spielberg").length;  
+};
+
+// Iteration 3: All rates average - Get the average of 
+// all rates with 2 decimals
+
+function ratesAverage(movies) {
+  if ( movies.length === 0) {
+      return 0
+  }
+  let total = movies.reduce((accumulator, currentValue) => {
+      return accumulator + (currentValue.rate || 0)
+  },0)
+  return Math.round((total / movies.length) * 100) / 100
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 
+function dramaMoviesRate(movies) {
+  let total = 0;  
+  total = movies.filter(drama => drama.genre.includes("Drama"))
+  return ratesAverage(total)
+}
+
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
+
+function orderByYear(movies) {
+  let Array = movies.sort((a, b) => {
+      return a.year > b.year ? 1 : -1;
+  })
+  return [...Array]
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 
-// BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-
-// BONUS - Iteration 8: Best yearly rate average - Best yearly rate average
+function orderAlphabetically(movies) {
+let title = []
+for (const movie of movies) {
+  title.push(movie.title);
+} title.sort()
+return title.slice(0,20)
+}
